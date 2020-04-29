d1581cd11b433ccc4bb4437b1a892399642d1219
Matheus Moreira <60182775+Phantochi@users.noreply.github.com>
2020-04-29 00:05:37 -0300

    Create space_remover.cs

---
+++ C#/space_remover.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace NoSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = Console.ReadLine();
+            x = x.Replace(" ", "");
+            Console.Write(x);
+        }
+    }
+}
