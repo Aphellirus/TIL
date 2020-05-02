706376727e1684c42bb410521bce2e926e5fd3d2
Matheus Moreira <60182775+Phantochi@users.noreply.github.com>
2020-05-02 00:41:02 -0300

    Create high_ground.cpp

---
+++ C++/high_ground.cpp
@@ -0,0 +1,16 @@
+include <iostream>
+using namespace std;
+
+int main()
+{
+
+/*This is just me messiang around with the dialogue
+between anakin and obi wan at the end of Star Wars Episose III: Revenge of the Sith */
+
+cout << "OBI WAN:It's over Anakin! I have the high ground!\n\n"; //Here Obi warns Anakin that he himself has the high ground LOL
+cout << "ANAKIN:You underestimate my power.\n\n"; //Anakin, you are clearly not listening, Obi is going to destroy u, man!!
+cout << "OBI WAN:Don't try it...\n\n"; //I'm offering you my mercy.... you weak sith.. or he was a jedi still?! 
+cout << "ANAKIN: AHHHHHHHHHHHHHHH, MY LEEEEEEEGS"; //Obi uses the almighty power of the high ground to cut off Anakin's limbs :((
+
+return 0;
+}
