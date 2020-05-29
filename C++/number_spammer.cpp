a13639c151ee6f4dd33d3c2485fc0f69490b8d9b
Matheus Moreira <60182775+Phantochi@users.noreply.github.com>
2020-05-28 21:42:09 -0300

    Create number_spammer.cpp

---
+++ C++/number_spammer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int num = 1;
+while (num < 69) {
+num += 1;
+    cout << "this has been displayed "  << num << " times " << endl;
+}
+
+    return 0;
+}
