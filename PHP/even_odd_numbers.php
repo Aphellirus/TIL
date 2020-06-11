05426399c4f29fd3a8c4ad515f088ce39f0613e8
Matheus Moreira <60182775+Phantochi@users.noreply.github.com>
2020-06-11 02:45:43 -0300

    Create even_odd_numbers.php

---
+++ PHP/even_odd_numbers.php
@@ -0,0 +1,17 @@
+<?php
+    for($a=0;$a<=10;$a++){
+    if($a%2==0){
+       echo $a;
+    }
+       echo "<br>";
+    }
+    
+    echo "<br>";
+    
+    for($b=0;$b<=10;$b++){
+    if($b%2!=0){
+       echo $b;
+    }
+       echo "<br>";
+    }
+?>
