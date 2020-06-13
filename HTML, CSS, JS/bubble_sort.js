0eda136c0dc3a444ea6646c3bbafac57d8e3ed70
Matheus Moreira <60182775+Phantochi@users.noreply.github.com>
2020-06-12 23:01:52 -0300

    Create bubble_sort.js

---
+++ HTML, CSS, JS/bubble_sort.js
@@ -0,0 +1,19 @@
+let bubbleSort = (inputArr) => {
+    let len = inputArr.length;
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < len; i++) {
+            if (inputArr[i] > inputArr[i + 1]) {
+                let tmp = inputArr[i];
+                inputArr[i] = inputArr[i + 1];
+                inputArr[i + 1] = tmp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+    return inputArr;
+};
+
+let sampleArray = [10,0,20,1,2,4,1,4,6,7,82,70,30];
+console.log(bubbleSort(sampleArray));
