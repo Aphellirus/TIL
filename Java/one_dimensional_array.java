5c5966cf9c4548153b0d5c15c1776f5b9e4aeb4a
Matheus Moreira <60182775+Phantochi@users.noreply.github.com>
2020-06-15 01:51:51 -0300

    Create one_dimensional_array.java

---
+++ Java/one_dimensional_array.java
@@ -0,0 +1,24 @@
+class AccessOfArray
+
+{
+
+public static void main(String[] args) 
+
+{
+
+int[] number= {11, 22, 33, 44, 55, 66, 77, 88, 99, 100}; 
+
+int length = number.length; // get size of array
+
+for (int i = 0; i < length; i++) 
+
+{
+
+System.out.println("Index of element = " + i +" & Element is = " +number[i]);
+
+}
+
+}
+
+}
+
