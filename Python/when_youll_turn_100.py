2de1e4ebcd0b431b6f73283281d7df88e505ad71
Matheus Moreira <60182775+Phantochi@users.noreply.github.com>
2020-06-29 12:44:09 -0300

    Create when_youll_turn_100.py

---
+++ Python/when_youll_turn_100.py
@@ -0,0 +1,5 @@
+current_year = int(2020)
+name = input("What is your name?" )
+age = int(input("How old are you? "))
+century_road = current_year - age + 100
+print ("hello again " + str(name) + ", you'll turn 100 on " + str(century_road)) 
