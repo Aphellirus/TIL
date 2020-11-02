717a5eaa5c5a2fbbb94b54d2c4ecaa10f0aab212
Matheus Moreira <60182775+Phantochi@users.noreply.github.com>
2020-11-01 22:12:28 -0300

    Create nested_loop.java

---
+++ Java/nested_loop.java
@@ -0,0 +1,13 @@
+public class Program
+{
+    public static void main(String[] args) {
+        for (int i=1;i<=9;i=i+2)
+        {
+            for(int j=i;j<=9;j=j+2)
+            {
+               System.out.print(j+"");
+            }
+        System.out.println();
+        }
+    }
+}
