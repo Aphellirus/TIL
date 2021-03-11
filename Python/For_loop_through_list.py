5c420fdcb9ae13af137546ed7945fb2dcab77031
Matheus Moreira <60182775+Neptuos@users.noreply.github.com>
2021-03-11 17:44:06 -0300

    Create For_loop_through_list.py

---
+++ Python/For_loop_through_list.py
@@ -0,0 +1,7 @@
+items = ['chairs' , 'machine' , 'couch' , 'trees' , 'bikes']
+
+for i, item in zip(range(len(items)), items):
+    print(i, item)
+
+
+#this uses a for loop to iterate through a list of tuples containing the index and value of each item in the list.
