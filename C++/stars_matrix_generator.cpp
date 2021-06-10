e661b96741f25f241a91b3d7bcf40fdec7eb5874
Matheus Moreira <60182775+Neptuos@users.noreply.github.com>
2021-06-10 11:39:02 -0300

    Create stars_matrix_generator.cpp

---
+++ C++/stars_matrix_generator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i,j,size;
+    cout<<"enter the size of the matrix\n";
+    cin>>size;
+    for (i=1;i<=size;i++){
+        for (j=1;j<=size;j++){
+            cout<<"* ";
+        }
+            cout<<"\n";
+        
+    }
+    
+    return 0;
+}
