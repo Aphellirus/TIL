a5316a043b6f9d3c7a1ec6e56c06e18f19ee573f
Matheus Moreira <60182775+Hibukim@users.noreply.github.com>
2021-08-22 17:27:14 -0300

    Create implicit_casting.java

---
+++ Java/implicit_casting.java
@@ -0,0 +1,14 @@
+public class Main {
+  
+  public static void main(string[] args) {
+  // implicit casting
+  short x;
+  int y;
+    
+  x = 1;
+  y = x + 2;
+  
+  System.out.println(y);
+  
+  }
+}
