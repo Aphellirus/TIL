759c0ba971c9fbcda7cb9c82eea88c7d53deedec
Matheus Moreira <60182775+Hibukim@users.noreply.github.com>
2021-08-25 06:23:46 -0300

    Create testing_do_while_in_4_different_ways.java

---
+++ Java/testing_do_while_in_4_different_ways.java
@@ -0,0 +1,31 @@
+public class Program{
+    public static void main(String[] args) {
+    
+   // .1
+      int x1 = 1;
+      do{System.out.println(x1);
+      x1++;}
+      while(x1 < 5);
+      System.out.println();
+      
+   // .2 
+      int x2 = 1;
+      do{System.out.println(x2);
+      x2++;}
+      while(x2 <= 5);
+      System.out.println();
+      
+   // .3 
+      int x3 = 1;
+      do{x3++;
+      System.out.println(x3);}
+      while(x3 < 5);       
+      System.out.println();
+      
+   // .4 
+      int x4= 1;
+      do{x4++;
+      System.out.println(x4);}
+      while(x4 <= 5);
+    }
+}
