525263940ade0bd1b820fc40ea2e336cf9d4b398
Matheus Moreira <60182775+Hibukim@users.noreply.github.com>
2021-09-06 16:50:23 -0300

    Create operators.go

---
+++ Golang/operators.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+  x := 25
+  y := 3
+      
+  res := x + y
+  fmt.Println(res)
+      
+  res = x - y
+  fmt.Println(res)
+      
+  res = x * y
+  fmt.Println(res)
+      
+  res = x / y
+  fmt.Println(res)
+
+  res = x % y
+  fmt.Println(res) 
+}
