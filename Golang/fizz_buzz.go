0d61ccd3227b5a8ad3bc247c08e0048807979fb3
Matheus Moreira <60182775+Hibukim@users.noreply.github.com>
2021-09-08 15:28:21 -0300

    Create fizz_buzz.go

---
+++ Golang/fizz_buzz.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+  var n int
+  fmt.Scanln(&n)
+  for i:=1; i<=n; i++{
+      if (i%3!=0){
+          if(i%5!=0){
+              fmt.Println(i)
+          }else {
+              fmt.Println("Buzz") 
+           }
+      }else if(i%5!=0){
+          fmt.Println("Fizz")
+            }else {
+           fmt.Println("FizzBuz")
+       }
+  }
+} 
