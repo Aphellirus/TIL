eeab5872b97d4c05341ca7cc8972da636a2dfa2e
Matheus Moreira <60182775+Hibukim@users.noreply.github.com>
2021-09-13 13:41:01 -0300

    Create area_of_a_circle.go

---
+++ Golang/area_of_a_circle.go
@@ -0,0 +1,18 @@
+package main                                                               	 
+import (                                                                   	 
+	"fmt"                                                                  	 
+		"math"                                                                 	 
+		)                                                                          	 
+func main() {                                                              	 
+	var radius int                                                         	 
+		var area float32                                                       	 
+	fmt.Println("Finding the area of a circle")                                    	 
+		fmt.Println("--------------------")                                    	 
+			fmt.Print("Enter radius: ")                                            	 
+				fmt.Scanf("%d", &radius)                                               	 
+	area = math.Pi * (float32(radius) * float32(radius))                   	 
+		fmt.Printf("Circle Area: %.2f\n", area)                                	 
+		}
+
+
+//input the radius, e.g. 2
