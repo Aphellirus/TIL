a549a8bba45307ca4031957f56e104c7c9cd9cf2
Matheus Moreira <60182775+Hibukim@users.noreply.github.com>
2021-10-13 00:03:42 -0300

    Create palindromic_pattern.cpp

---
+++ C++/palindromic_pattern.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+// Main Program
+int main() {
+	int n;
+
+	// Taking Input 😊
+	cout<<"Enter a Number : ";
+	cin>>n;
+	cout<<endl;
+
+	//Outer loop for no. of rows
+	for(int i = 1; i <= n; i++) {
+		// For spaces
+		for (int k = (n - i); k > 0; k--) {
+			cout<<"  ";
+		}
+		// First half of the palindrome
+		for (int j = i; j >= 2; j--) {
+			cout<<j<<" ";
+		}
+		//Second Half of the Palindrome
+		for (int j = 1; j <= i; j++) {
+			cout<<j<<" ";
+		}
+		cout<<endl;
+	}
+}
