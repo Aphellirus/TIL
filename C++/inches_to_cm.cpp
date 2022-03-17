431b8ca801e614f113b5760e4928b212ab412a04
Matheus Moreira <60182775+Hibukim@users.noreply.github.com>
2022-03-17 16:33:22 -0300

    Create inches_to_cm.cpp

---
+++ C++/inches_to_cm.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{float  inches,cm;
+cout <<"Enter how many inches :";
+cin>>inches ;
+cm = 2.54*inches;
+cout <<"There are "<<cm<<"centimeter in "<<inches<<"inches";
+    return 0;
+}
