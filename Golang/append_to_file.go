2963af36ee69541c42d1c9a1fb5bff682dfa80a0
Matheus Moreira <60182775+Phantochi@users.noreply.github.com>
2022-10-29 19:41:46 -0300

    Create append_to_file.go

---
+++ Golang/append_to_file.go
@@ -0,0 +1,10 @@
+f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+if err != nil {
+    panic(err)
+}
+
+defer f.Close()
+
+if _, err = f.WriteString(text); err != nil {
+    panic(err)
+}
