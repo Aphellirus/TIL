0cbefb4a1c945f199278bcadf6f06db989d97187
Matheus Moreira <60182775+Phantochi@users.noreply.github.com>
2022-11-01 22:40:12 -0300

    Create user_input.cs

---
+++ C#/user_input.cs
@@ -0,0 +1,7 @@
+Console.Write("Enter your name:");
+           string name = Console.ReadLine();
+            Console.Write("Enter your age:");
+            string age = Console.ReadLine();
+           
+            Console.WriteLine("Hello" + name + " you are " + age);
+			Console.ReadLine();
