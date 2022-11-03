ffb41931f871d0f145a12bb4bbe54b792fa9c62c
Matheus Moreira <60182775+Phantochi@users.noreply.github.com>
2022-11-02 22:25:35 -0300

    Create reverse_list.kt

---
+++ Kotlin/reverse_list.kt
@@ -0,0 +1,9 @@
+val reversedList = oldList.reversed()
+
+// Example
+fun main() {
+ 	val list = listOf("A", "B", "C")
+    println(list)
+    val reversed = list.reversed()
+    println(reversed)
+}
