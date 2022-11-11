5aec92c6b39dad28b9e0dc4c4f5979207e93a79c
Matheus Moreira <60182775+Aphellirus@users.noreply.github.com>
2022-11-11 10:56:50 -0300

    Create message_box.cs

---
+++ C#/message_box.cs
@@ -0,0 +1,9 @@
+DialogResult dialogResult = MessageBox.Show("Sure", "Some Title", MessageBoxButtons.YesNo);
+if(dialogResult == DialogResult.Yes)
+{
+    //do something
+}
+else if (dialogResult == DialogResult.No)
+{
+    //do something else
+}
