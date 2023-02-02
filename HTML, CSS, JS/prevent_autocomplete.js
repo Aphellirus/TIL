0e1847c67a2cc39ac992f420e4256ce3b56acc84
Matheus Moreira <60182775+Aphellirus@users.noreply.github.com>
2023-02-02 13:00:48 -0300

    Create prevent_autocomplete.js

---
+++ HTML, CSS, JS/prevent_autocomplete.js
@@ -0,0 +1,6 @@
+<form method="post" action="/post/" autocomplete="off">
+	<!-- The entire form has autocomplete disabled. -->
+</form>
+
+<!-- or you turn it off for just one input -->
+<input type="text" autocomplete="off">
