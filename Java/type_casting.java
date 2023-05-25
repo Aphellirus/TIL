009d0b8c6643bfea10e8384ae039070227f9ea6d
Matheus Moreira <60182775+Aphellirus@users.noreply.github.com>
2023-05-25 11:30:18 -0300

    Create type_casting.java

---
+++ Java/type_casting.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[]args) {
+
+       int IntAmount = 5;
+       double doubleamount = IntAmount; // Automatic casting: int to double
+        System.out.println(doubleamount);
+
+    }
+}
