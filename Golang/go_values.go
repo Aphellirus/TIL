1be59e8c776ce0e958ffeb3f159b558a6c27de76
Neptuos <matheus.goryo@gmail.com>
2023-10-16 13:29:06 -0300

    playing around with values in golang

---
+++ Golang/go_values.go
@@ -0,0 +1,18 @@
+/*
+playing around with some of the value types in golang including strings, integers, floats, booleans, etc
+*/
+
+package main
+
+func main() {
+
+	println("go" + "lang")
+
+	println("1+1 =", 1+1)
+	println("7.0/3.0 =", 7.0/3.0)
+
+	println(true && false)
+	println(true || false)
+	println(!true)
+
+}
